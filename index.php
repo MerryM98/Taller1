018a4c6c3215d96bae071f1d99fabe179b55cbd4
MerryM98 <61071116+MerryM98@users.noreply.github.com>
2020-02-21 21:16:24 -0500

    Punto1

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
-echo("Hola");
+echo("Taller 1 Merilian Mejia Palacio <br>");
+echo("Punto 1<br>");
+
+$numero1=40;
+$numero2=50;
+
+
+$suma=$numero1+$numero2;
+$resta=$numero1-$numero2;
+$multiplicacion=$numero1*$numero2;
+$division=$numero1/$numero2;
+
+echo("<br>La suma de:".$numero1."+".$numero2."=".$suma);
+
+echo("<br>La resta de:".$numero1."-".$numero2."=".$resta);
+
+echo("<br>La Multiplicación de:".$numero1."*".$numero2."=".$multiplicacion); 
+
+echo("<br>La división de:".$numero1."/".$numero2."=".$division);
+
 
 
 
